e95ebde2f47cff03ab8e8f5d75f1ad0873e50a1a
Furkan Köykıran <furkankoykiran@gmail.com>
2023-10-11 20:02:46 +0300

    * added even slot_counts.

--- 001 - Plinko/Plinko.py
+++ 001 - Plinko/Plinko.py
@@ -1,4 +1,3 @@
-
 import random
 
 ball_count = input(f"Top sayısını giriniz: ")
@@ -6,7 +5,6 @@ ball_count = int(ball_count) if ball_count.isdigit() else exit("Top sayısı say
 
 slot_count = input(f"Yuva sayısını giriniz: ")
 slot_count = int(slot_count) if slot_count.isdigit() else exit("Yuva sayısı sayısal değer olmalı.")
-slot_count = slot_count if slot_count % 2 == 1 else exit("Yuva sayısı tek sayı olmalı.")
 
 # Topların düştüğü yuvaları tutmak için bir liste oluşturuyoruz.
 positions = []
@@ -14,7 +12,7 @@ positions = []
 # 1'den başlayıp ball_count'a kadar tüm topları işliyoruz.
 for ball_number in range(1, ball_count + 1):
     # Topların girebileceği yuva sayısının en ortasını buluyoruz. (Piramit olacağı için bu şekilde)
-    middle = (slot_count // 2) + 1
+    middle = (slot_count // 2) + 1 if slot_count % 2 == 1 else (slot_count / 2) + 0.5
     # Slot sayısından 1 az kadar döngüye giriyoruz her döngüde rastgele bir yöne doğru hareket ediyoruz.
     for slot_number in range(1, slot_count):
         move = random.choice([-0.5, 0.5]) # -0.5 sola, 0.5 sağa hareket ediyor.
@@ -23,7 +21,7 @@ for ball_number in range(1, ball_count + 1):
             print(f"Top {ball_number} sağa gitti.")
         else:
             print(f"Top {ball_number} sola gitti.")
-    print(f"Top {int(middle)} numaralı yuvaya düştü.\n")
+    print(f"Top {ball_number} {int(middle)} numaralı yuvaya düştü.\n")
     positions.append(middle)
 
 # Topların düştüğü yuvaları sayıyoruz.
